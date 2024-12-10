c050b6b58bde22f551d9a4d9323714ad05bb400b
B1tGuzok <159481660+B1tGuzok@users.noreply.github.com>
2024-12-10 23:41:50 +0300

    New Buttons and Transitions
    Updated sprites and scene transition system, added new buttons, removed unnecessary ones

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,15 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
-    public int sceneNumber;
+    [SerializeField] GameObject homeButton;
+    [SerializeField] GameObject modeButtons;
+    [SerializeField] GameObject lvlButtons;
+    [SerializeField] GameObject topButton;
 
-    public void Transition()
+    public void OpenHome() { SceneManager.LoadScene("Menu"); }
+
+    public void OpenTop() { SceneManager.LoadScene("Top"); }
+
+    public void OpenLvl1() { SceneManager.LoadScene("Lvl1"); }
+
+    public void OpenButchery() { SceneManager.LoadScene("Game"); }
+
+    public void OpenExit() { SceneManager.LoadScene("BeforeEnd"); }
+
+    public void ShowLvlButtons()
     {
-        SceneManager.LoadScene(sceneNumber);
+        modeButtons.SetActive(false);
+        topButton.SetActive(false);
+        lvlButtons.SetActive(true);
+        homeButton.SetActive(true);
+    }
+
+    public void HideLvlButtons()
+    {
+        modeButtons.SetActive(true);
+        topButton.SetActive(true);
+        lvlButtons.SetActive(false);
+        homeButton.SetActive(false);
     }
 
     public void Exit()
